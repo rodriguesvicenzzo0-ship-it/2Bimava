bf586fb999121173b1c67f34251f54073e2224a9
lorranelias514-lab <lorranelias514@gmail.com>
2026-06-01 15:09:27 +0000

    v1

--- calc_imc.py
+++ calc_imc.py
@@ -1,1 +1,6 @@
-
+#---lorran elias da conceição---
+# Função: calcular_imc(p,a)
+#Recebe peso (p) e altura (a), retorna o resultado da conta: peso/altura²
+def calcular_imc(p,a):
+    resultado = p/ (a ** 2)
+    return resultado
