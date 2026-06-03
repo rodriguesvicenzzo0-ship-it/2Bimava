967213905476f209e3d1124efbc01ca680be7dd3
rodriguesvicenzzo0-ship-it <rodriguesvicenzzo0@gmail.com>
2026-06-03 12:23:14 +0000

    v5

--- calc_imc.py
+++ calc_imc.py
@@ -16,4 +16,30 @@ def gerar_aviso(status):
     return("Parabéns! Seu peso está adequando para sua altura! Mantenha os hábitos saúdaveis, com alimentação saudável e praticando atividades físicas")
  else:
     return("Seu IMC indica que você está acima do peso! Recomenda-se buscar uma dieta balenceada e incluir exercícios físicos em sua rotina")
- 
+# =====================================================================
+#vicenzzo4 (Engenheiro de Integração)
+# =====================================================================
+# from logica import calcular_imc
+# from classificacao import classificar
+# from conteudo import gerar_aviso
+
+def fluxo_principal():
+    print("=== INICIANDO SISTEMA DE ANÁLISE DE IMC ===")
+    
+    try:
+        peso = float(input("Digite o seu peso (em kg): "))
+        altura = float(input("Digite a sua altura (em metros): "))
+        imc_calculado = calcular_imc(peso, altura) 
+        status_peso = classificar(imc_calculado)
+        dica_saude = gerar_aviso(status_peso)
+        
+        print("\n-------------------------------------------")
+        print(f"-> Seu IMC é: {imc_calculado:.2f}")
+        print(f"-> Diagnóstico: {status_peso}")
+        print(f"-> Recomendação: {dica_saude}")
+        print("---------------------------------------------")
+        
+    except ValueError:
+        print("\n[ERRO] Por favor, digite números válidos usando ponto (.) para decimais.")
+if __name__ == "__main__":
+    fluxo_principal():
